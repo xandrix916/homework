23debac2091c96b582cad33ecd535c690decef25
xandrix916 <daniel_aran04@mail.ru>
2023-10-12 02:33:29 +0700

    Исправление checkstyle #7

--- src/main/java/edu/hw1/Problem1.java
+++ src/main/java/edu/hw1/Problem1.java
@@ -3,24 +3,30 @@ package edu.hw1;
 import static java.lang.Character.getNumericValue;
 
 public class Problem1 {
+    private final int ten = 10;
+    private final int five = 5;
+    private final int three = 3;
+    private final int amountOfSecondsInMinute = 60;
+
     public int minutesToSeconds(String time) {
-        int minutes = 0, seconds = 0;
-        if (time.length() < 5 || time.indexOf(':') == -1 || time.length() - time.indexOf(':') != 3)
+        int minutes = 0;
+        int seconds = 0;
+        if (time.length() < five || time.indexOf(':') == -1 || time.length() - time.indexOf(':') != three) {
             return -1;
+        }
         for (int i = 0; i < time.indexOf(':'); i++) {
-            if (!Character.isDigit(time.charAt(i)))
+            if (!Character.isDigit(time.charAt(i))) {
                 return -1;
-            minutes += getNumericValue(time.charAt(i)) * (int)Math.pow(10, time.indexOf(':') - i - 1);
+            }
+            minutes += getNumericValue(time.charAt(i)) * (int)Math.pow(ten, time.indexOf(':') - i - 1);
         }
         for (int i = time.indexOf(':') + 1; i < time.length(); i++) {
-            if (!Character.isDigit(time.charAt(i)))
+            if (!Character.isDigit(time.charAt(i))) {
                 return -1;
-            seconds += getNumericValue(time.charAt(i)) * (int)Math.pow(10, time.length() - i - 1);
+            }
+            seconds += getNumericValue(time.charAt(i)) * (int)Math.pow(ten, time.length() - i - 1);
         }
-        if (seconds >= 60)
-            return -1;
-        else
-            return minutes * 60 + seconds;
+        return seconds >= amountOfSecondsInMinute ? -1 : minutes * amountOfSecondsInMinute + seconds;
     }
 }
 
--- src/main/java/edu/hw1/Problem8.java
+++ src/main/java/edu/hw1/Problem8.java
@@ -3,20 +3,22 @@ package edu.hw1;
 public class Problem8 {
     private final int boardBound = 8;
     private final int twoBlocksDown = -2;
+
     private int[][] allPossibleWays(int letter, int digit) {
         int[][] allWays = new int[boardBound][2];
         int counter = 0;
         int[][] possibleCombinations = {{twoBlocksDown, -1}, {twoBlocksDown, 1}, {-1, twoBlocksDown}, {-1, 2}, {1, 2},
             {1, twoBlocksDown}, {2, 1}, {2, -1}};
         for (var comb: possibleCombinations) {
-            int dig = comb[0], let = comb[1];
-            if (0 <= letter+let && letter + let < boardBound && 0 <= digit + dig && digit + dig < boardBound) {
+            int dig = comb[0];
+            int let = comb[1];
+            if (0 <= letter + let && letter + let < boardBound && 0 <= digit + dig && digit + dig < boardBound) {
                 allWays[counter][1] = letter + let;
                 allWays[counter][0] = digit + dig;
                 counter++;
             }
         }
-        if (counter < 8) {
+        if (counter < boardBound) {
             int[][] finalAllWays = new int[counter][2];
             for (int i = 0; i < counter; i++) {
                 finalAllWays[i][0] = allWays[i][0];
@@ -33,8 +35,9 @@ public class Problem8 {
                 if (board[dig][let] == 1) {
                     int[][] waysToGo = allPossibleWays(let, dig);
                     for (var pos: waysToGo) {
-                        if (board[pos[0]][pos[1]] == 1)
+                        if (board[pos[0]][pos[1]] == 1) {
                             return false;
+                        }
                     }
                 }
             }
