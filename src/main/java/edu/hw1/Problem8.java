0ca08300f5a795fddc83746814de699c7a74d023
xandrix916 <daniel_aran04@mail.ru>
2023-10-12 02:25:43 +0700

    Исправление checkstyle #6

--- src/main/java/edu/hw1/Problem8.java
+++ src/main/java/edu/hw1/Problem8.java
@@ -1,21 +1,22 @@
 package edu.hw1;
 
 public class Problem8 {
-
-    private int[][] allPossibleWays(int letter, int digit){
-        int[][] allWays = new int[8][2];
+    private final int boardBound = 8;
+    private final int twoBlocksDown = -2;
+    private int[][] allPossibleWays(int letter, int digit) {
+        int[][] allWays = new int[boardBound][2];
         int counter = 0;
-        int[][] possibleCombinations = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, 2}, {1, -2}, {2, 1}, {2, -1}};
-        for (var comb:
-             possibleCombinations) {
+        int[][] possibleCombinations = {{twoBlocksDown, -1}, {twoBlocksDown, 1}, {-1, twoBlocksDown}, {-1, 2}, {1, 2},
+            {1, twoBlocksDown}, {2, 1}, {2, -1}};
+        for (var comb: possibleCombinations) {
             int dig = comb[0], let = comb[1];
-            if (0 <= letter+let && letter + let < 8 && 0 <= digit + dig && digit + dig < 8) {
+            if (0 <= letter+let && letter + let < boardBound && 0 <= digit + dig && digit + dig < boardBound) {
                 allWays[counter][1] = letter + let;
                 allWays[counter][0] = digit + dig;
                 counter++;
             }
         }
-        if (counter < 8){
+        if (counter < 8) {
             int[][] finalAllWays = new int[counter][2];
             for (int i = 0; i < counter; i++) {
                 finalAllWays[i][0] = allWays[i][0];
@@ -26,14 +27,12 @@ public class Problem8 {
         return allWays;
     }
 
-
     public boolean knightBoardCapture(int[][] board) {
-        for (int dig = 0; dig < 8; dig++) {
-            for (int let = 0; let < 8; let++) {
+        for (int dig = 0; dig < boardBound; dig++) {
+            for (int let = 0; let < boardBound; let++) {
                 if (board[dig][let] == 1) {
                     int[][] waysToGo = allPossibleWays(let, dig);
-                    for (var pos:
-                         waysToGo) {
+                    for (var pos: waysToGo) {
                         if (board[pos[0]][pos[1]] == 1)
                             return false;
                     }
