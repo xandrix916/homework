0c2ec6634b17bc06cf9e742a7b6a212eb87fa0de
xandrix916 <daniel_aran04@mail.ru>
2023-10-12 02:21:15 +0700

    Исправление checkstyle #5

--- src/main/java/edu/hw1/Problem4.java
+++ src/main/java/edu/hw1/Problem4.java
@@ -2,20 +2,22 @@ package edu.hw1;
 
 public class Problem4 {
     public String fixString(String string) {
-        if (string.isEmpty())
+        if (string.isEmpty()) {
             return "";
+        }
         char[] array = string.toCharArray();
-        string = "";
-        for (int i = 0; i < (array.length - (array.length)%2); i+= 2) {
+        String resultString = "";
+        for (int i = 0; i < (array.length - (array.length) % 2); i += 2) {
             char temp;
             temp = array[i];
-            array[i] = array[i+1];
-            array[i+1] = temp;
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
         }
-        StringBuilder stringBuilder = new StringBuilder(string);
-        for (var c: array)
+        StringBuilder stringBuilder = new StringBuilder(resultString);
+        for (var c: array) {
             stringBuilder.append(c);
-        string = stringBuilder.toString();
-        return string;
+        }
+        resultString = stringBuilder.toString();
+        return resultString;
     }
 }
