3accbc7377c7ea1ea994e18d62371cd68dd57e81
xandrix916 <daniel_aran04@mail.ru>
2023-10-12 21:28:27 +0700

    Переделан код для теста

--- src/test/java/edu/hw1/Problem0Test.java
+++ src/test/java/edu/hw1/Problem0Test.java
@@ -11,6 +11,7 @@ class Problem0Test {
     void inputOutputTest() throws Exception {
         problem0 = new Problem0();
         String helloWorld = tapSystemErr(() -> problem0.helloWorld());
-        Assertions.assertEquals("INFO: Привет, мир!", helloWorld.substring(helloWorld.indexOf("\n")+1, helloWorld.length()-2));
+        String infoString = helloWorld.substring(helloWorld.indexOf("\n")+1, helloWorld.lastIndexOf("\r"));
+        Assertions.assertEquals("INFO: Привет, мир!", infoString);
     }
 }
