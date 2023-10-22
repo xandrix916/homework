42c67e091fd3f357e082ad2c2e592c7da9bf2259
xandrix916 <daniel_aran04@mail.ru>
2023-10-23 01:12:07 +0700

    Initial commit Решены две задачи, к каждой сделаны тесты

---
+++ src/main/java/edu/hw3/Problem1.java
@@ -0,0 +1,28 @@
+package edu.hw3;
+
+public class Problem1 {
+    private static final int LOWER_CASE_CONSTANT = 219;
+
+    private static final int UPPER_CASE_CONSTANT = 155;
+    private char mirrorValue(char c) {
+        if (Character.isLowerCase(c)) {
+            return (char) (LOWER_CASE_CONSTANT - (int) c);
+        }
+        if (Character.isUpperCase(c)) {
+            return (char) (UPPER_CASE_CONSTANT - (int) c);
+        }
+        else {
+            return c;
+        }
+    }
+
+
+    public String atbash(String originalString) {
+        char[] charArray = originalString.toCharArray();
+        StringBuilder cypheredString = new StringBuilder();
+        for (var c: charArray) {
+            cypheredString.append(mirrorValue(c));
+        }
+        return cypheredString.toString();
+    }
+}
