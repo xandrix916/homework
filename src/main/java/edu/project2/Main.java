2205f157f92ce185d689a6fcb2d0e0d4f4e29dc2
xandrix916 <daniel_aran04@mail.ru>
2023-10-31 00:58:48 +0700

    v 0.0.1 добавлено хранение лабиринта в виде вершин

---
+++ src/main/java/edu/project2/Main.java
@@ -0,0 +1,10 @@
+package edu.project2;
+
+public class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        Maze maze = new Maze(3, 3);
+    }
+}
--- src/main/java/edu/project2/Maze.java
+++ src/main/java/edu/project2/Maze.java
@@ -5,10 +5,43 @@ public final class Maze {
     private final int mazeWidth;
     private final Cell[][] maze;
 
+    private final Vertex[][] vertices;
+
+    private void initMaze() {
+        for (int i = 0; i < mazeHeight; i++) {
+            for (int j = 0; j < mazeWidth; j++) {
+                maze[i][j] = new Cell(new Cell.Location(i, j));
+            }
+        }
+    }
+
+    private void initVertices() {
+        for (int i = 0; i <= mazeHeight; i++) {
+            for (int j = 0; j <= mazeWidth; j++) {
+                vertices[i][j] = new Vertex(i, j);
+            }
+        }
+    }
+
+    private void arrangeEdges() {
+        for (int i = 0; i <= mazeHeight; i++) {
+            for (int j = 0; j <= mazeWidth; j++) {
+                vertices[i][j].updateEdges(vertices);
+            }
+        }
+    }
+
+
     public Maze(int mazeHeight, int mazeWidth) {
         this.mazeHeight = mazeHeight;
         this.mazeWidth = mazeWidth;
+
         this.maze = new Cell[mazeHeight][mazeWidth];
+        initMaze();
+
+        this.vertices = new Vertex[mazeHeight + 1][mazeWidth + 1];
+        initVertices();
+        arrangeEdges();
     }
 
     public int getMazeHeight() {
