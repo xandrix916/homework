0f303f1c7bae8c8269d6174209f0fd4d3ad6ebc3
xandrix916 <daniel_aran04@mail.ru>
2023-11-14 15:54:58 +0700

    Revert "Убраны файлы из веток hw4 и project2"
    This reverts commit ba31a3cbe30396f7de92c748c9f3639781ee7070.

---
+++ src/main/java/edu/project2/Main.java
@@ -0,0 +1,26 @@
+package edu.project2;
+
+public class Main {
+    private Main() {
+    }
+
+    @SuppressWarnings("ParameterNumber")
+    public static String testGenerate(int[] edgeOrder, int n, int m, int[] startIndex, int[] endIndex,
+        Cell.WallSide startWallSide, Cell.WallSide endWallSide, boolean solve) {
+        Generator kruskal = new Kruskal(edgeOrder);
+        Maze maze = kruskal.generate(n, m);
+        Cell.Location startCell = maze.getCellByCoordinates(startIndex[0], startIndex[1]).getLocation();
+        Cell.Location endCell = maze.getCellByCoordinates(endIndex[0], endIndex[1]).getLocation();
+        maze.pickExitsManually(startCell, endCell, startWallSide, endWallSide);
+        maze.render();
+        if (solve) {
+            Solver tremaux = new TremauxSolver(maze);
+            maze.setSolver(tremaux);
+            maze.getRenderer().setNoMarks(true);
+            maze.render();
+        }
+        return maze.getRenderer().toString();
+    }
+
+    public static void main(String[] args) {}
+}
---
+++ src/main/java/edu/project2/Vertex.java
@@ -0,0 +1,87 @@
+package edu.project2;
+
+import java.util.EnumMap;
+
+public class Vertex {
+    private final int vertexRow; //0..n
+    private final int vertexCol; //0..m
+
+    private final EnumMap<Cell.WallSide, Edge> edgeMap = new EnumMap<>(Cell.WallSide.class);
+
+    public Vertex(int vertexRow, int vertexCol) {
+        this.vertexRow = vertexRow;
+        this.vertexCol = vertexCol;
+
+        edgeMap.put(Cell.WallSide.NORTH, null);
+        edgeMap.put(Cell.WallSide.SOUTH, null);
+        edgeMap.put(Cell.WallSide.EAST, null);
+        edgeMap.put(Cell.WallSide.WEST, null);
+    }
+
+    @SuppressWarnings("ReturnCount")
+    private boolean checkEdge(Cell.WallSide wallSide, Vertex[][] vertices) {
+        Edge adjacentEdge = null;
+        switch (wallSide) {
+            case NORTH -> {
+                if (vertexRow - 1 < 0) {
+                    return true; //means there's nothing to change
+                }
+                adjacentEdge = vertices[vertexRow - 1][vertexCol].edgeMap.get(Cell.WallSide.SOUTH);
+            }
+            case SOUTH -> {
+                if (vertexRow + 1 >= vertices.length) {
+                    return true;
+                }
+                adjacentEdge = vertices[vertexRow + 1][vertexCol].edgeMap.get(Cell.WallSide.NORTH);
+            }
+            case WEST -> {
+                if (vertexCol - 1 < 0) {
+                    return true;
+                }
+                adjacentEdge = vertices[vertexRow][vertexCol - 1].edgeMap.get(Cell.WallSide.EAST);
+            }
+            case EAST -> {
+                if (vertexCol + 1 >= vertices[0].length) {
+                    return true;
+                }
+                adjacentEdge = vertices[vertexRow][vertexCol + 1].edgeMap.get(Cell.WallSide.WEST);
+            }
+            default -> {
+
+            }
+        }
+        if (adjacentEdge != null) {
+            this.edgeMap.put(wallSide, adjacentEdge);
+            return true;
+        }
+        return false;
+    }
+
+
+
+    public void updateEdges(Vertex[][] vertices) {
+        for (var side: edgeMap.keySet()) {
+            if (!checkEdge(side, vertices)) {
+                Vertex secondVertex = switch (side) {
+                    case NORTH -> vertices[vertexRow - 1][vertexCol];
+                    case SOUTH -> vertices[vertexRow + 1][vertexCol];
+                    case WEST -> vertices[vertexRow][vertexCol - 1];
+                    case EAST -> vertices[vertexRow][vertexCol + 1];
+                };
+                this.edgeMap.put(side, new Edge(this, secondVertex));
+            }
+        }
+   }
+
+   public Edge getEdgeBySide(Cell.WallSide wallSide) {
+        return edgeMap.get(wallSide);
+   }
+
+    public int getVertexCol() {
+        return vertexCol;
+    }
+
+    public int getVertexRow() {
+        return vertexRow;
+    }
+}
